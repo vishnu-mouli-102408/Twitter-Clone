cf1b15a3a48d5bc7693e0d39fb4d58061569322a
Vishnu Mouli <vishnumouli0@gmail.com>
2024-01-15 09:58:09 +0530

    fix the bug

--- src/app/_components/NewTweetForm.tsx
+++ src/app/_components/NewTweetForm.tsx
@@ -3,45 +3,40 @@ import React, { useEffect, useState } from "react";
 import CustomButton from "./CustomButton";
 import ProfileImage from "./ProfileImage";
 import { sessionDetails } from "../actions";
-import { DefaultUser } from "next-auth";
+import type { DefaultUser } from "next-auth"; // Use `import type` for types
 
 const NewTweetForm = () => {
   const [inputValue, setInputValue] = useState("");
-  const [session, setSession] = useState<DefaultUser>({
-    id: "",
-    name: "",
-    email: "",
-    image: "",
-  });
+  const [session, setSession] = useState<DefaultUser | null>(null); // Specify null as a possible type
   useEffect(() => {
     try {
       const sessionFunction = async () => {
-        const session = await sessionDetails();
-        console.log(session);
-        if (session?.user) {
-          setSession(session?.user);
+        const sessionData = await sessionDetails();
+        console.log(sessionData);
+        if (sessionData?.user) {
+          setSession(sessionData?.user);
         } else {
           return;
         }
       };
       sessionFunction();
     } catch (error) {
-      console.log("Error Occured", error);
+      console.log("Error Occurred", error);
     }
   }, []);
 
-  if (!session) return;
+  if (!session) return null; // Return null instead of undefined
 
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        <ProfileImage src={session?.image} />
+        <ProfileImage src={session.image} />
         <textarea
           style={{ height: 0 }}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
-          placeholder="What's on you mind today?"
+          placeholder="What's on your mind today?" // Corrected the typo in the placeholder
         />
       </div>
       <CustomButton className="self-end">Send</CustomButton>
